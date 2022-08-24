528332d356f4005c27ba4991ca358e0065319579
icrespocasajus <isaaccrespo@hotmail.com>
2022-08-24 10:50:28 +0200

    delete

--- R/LCMV-data.R
+++
@@ -1,5 +0,0 @@
-library(usethis)
-network.LCMV = readRDS(file="./network.LCMV.Rds")
-modulons.LCMV=readRDS(file="./TF.AUC.clusters.LCMV.subset.Rds")
-usethis::use_data(network.LCMV, overwrite = TRUE)
-usethis::use_data(modulons.LCMV, overwrite = TRUE)
